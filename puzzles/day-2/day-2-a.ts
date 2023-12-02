bdc5cbb10eb059c697272165ed8cfeaa881dd73b
Mohit Singh <mohitwildbeast@gmail.com>
2023-12-02 20:28:03 +0530

    day 2

--- puzzles/day-2/day-2-a.ts
+++ puzzles/day-2/day-2-a.ts
@@ -1,9 +1,40 @@
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
+const RED = 12;
+const GREEN = 13;
+const BLUE = 14;
+
 export async function day2a(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+  let sumOfValidGames = 0;
+  for (const line of data) {
+    sumOfValidGames += isValidGame(line);
+  }
+  return sumOfValidGames;
+}
+
+function isValidGame(line: string) {
+  const game = parseInt(line.substring(5, line.indexOf(':')));
+  line = line.substring(line.indexOf(':') + 2);
+  const records = line.split(';');
+  for (const record of records) {
+    const colorCubes = record.split(',').map((cube) => cube.trim());
+    for (const cube of colorCubes) {
+      const number = parseInt(cube.substring(0, cube.indexOf(' ')));
+      const color = cube.substring(cube.indexOf(' ') + 1);
+      if (!isValidColorCount(color, number)) return 0;
+    }
+  }
+  return game;
+}
+
+function isValidColorCount(color: string, count: number): boolean {
+  return (
+    (color === 'red' && count <= RED) ||
+    (color === 'green' && count <= GREEN) ||
+    (color === 'blue' && count <= BLUE)
+  );
 }
 
 const answer = await day2a();
--- puzzles/day-2/day-2-b.ts
+++ puzzles/day-2/day-2-b.ts
@@ -3,7 +3,33 @@ import chalk from 'chalk';
 
 export async function day2b(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+  let sumOfCubePowers = 0;
+  for (const line of data) {
+    sumOfCubePowers += powerOfCubes(line);
+  }
+  return sumOfCubePowers;
+}
+
+function powerOfCubes(line: string) {
+  const game = parseInt(line.substring(5, line.indexOf(':')));
+  line = line.substring(line.indexOf(':') + 2);
+  const minCubesRequired = { red: 0, green: 0, blue: 0 };
+  const records = line.split(';');
+  for (const record of records) {
+    const colorCubes = record.split(',').map((cube) => cube.trim());
+    for (const cube of colorCubes) {
+      const number = parseInt(cube.substring(0, cube.indexOf(' ')));
+      const color = cube.substring(cube.indexOf(' ') + 1);
+      if (color === 'red') {
+        minCubesRequired.red = Math.max(minCubesRequired.red, number);
+      } else if (color === 'green') {
+        minCubesRequired.green = Math.max(minCubesRequired.green, number);
+      } else if (color === 'blue') {
+        minCubesRequired.blue = Math.max(minCubesRequired.blue, number);
+      }
+    }
+  }
+  return minCubesRequired.red * minCubesRequired.blue * minCubesRequired.green;
 }
 
 const answer = await day2b();
