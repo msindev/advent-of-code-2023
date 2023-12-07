d9363640986924d91b38c3581814e24ef6fd7e38
Mohit Singh <mohitwildbeast@gmail.com>
2023-12-07 22:12:32 +0530

    day 4

--- puzzles/day-4/day-4-a.ts
+++ puzzles/day-4/day-4-a.ts
@@ -3,7 +3,32 @@ import chalk from 'chalk';
 
 export async function day4a(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+  let sum = 0;
+  for (const line of data) {
+    sum += getPoints(line);
+  }
+  return sum;
+}
+
+function getPoints(line: string) {
+  line = line.substring(line.indexOf(':') + 1);
+  const winningNumbers = line
+    .substring(0, line.indexOf('|'))
+    .trim()
+    .split(' ')
+    .filter((n) => n !== '');
+  const myNumbers = line
+    .substring(line.indexOf('|') + 1)
+    .trim()
+    .split(' ')
+    .filter((n) => n !== '');
+  let count = 0;
+  for (const number of myNumbers) {
+    if (winningNumbers.includes(number)) {
+      count += 1;
+    }
+  }
+  return count > 0 ? Math.pow(2, count - 1) : 0;
 }
 
 const answer = await day4a();
--- puzzles/day-4/day-4-b.ts
+++ puzzles/day-4/day-4-b.ts
@@ -3,7 +3,40 @@ import chalk from 'chalk';
 
 export async function day4b(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+  let matchingCardCount = [];
+  let cards = [];
+  for (const line of data) {
+    matchingCardCount.push(getPoints(line));
+    cards.push(1);
+  }
+  for (let i = 0; i < matchingCardCount.length; i++) {
+    const points = matchingCardCount[i];
+    for (let j = 0; j < points; j++) {
+      cards[i + j + 1] += cards[i];
+    }
+  }
+  return cards.reduce((a, b) => a + b, 0);
+}
+
+function getPoints(line: string) {
+  line = line.substring(line.indexOf(':') + 1);
+  const winningNumbers = line
+    .substring(0, line.indexOf('|'))
+    .trim()
+    .split(' ')
+    .filter((n) => n !== '');
+  const myNumbers = line
+    .substring(line.indexOf('|') + 1)
+    .trim()
+    .split(' ')
+    .filter((n) => n !== '');
+  let count = 0;
+  for (const number of myNumbers) {
+    if (winningNumbers.includes(number)) {
+      count += 1;
+    }
+  }
+  return count;
 }
 
 const answer = await day4b();
