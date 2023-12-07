7fd1ee40918d18ca6de083b4b8624a07fa732df4
Mohit Singh <mohitwildbeast@gmail.com>
2023-12-07 22:59:28 +0530

    day 6

--- puzzles/day-6/day-6-a.ts
+++ puzzles/day-6/day-6-a.ts
@@ -1,9 +1,46 @@
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
+interface Race {
+  time: number;
+  distance: number;
+}
+
+const races: Race[] = [];
+
 export async function day6a(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+  prepareData(data[0], data[1]);
+  let numberOfWays = 1;
+  for (const race of races) {
+    numberOfWays *= numberOfWaysToWinRace(race.time, race.distance);
+  }
+  return numberOfWays;
+}
+
+function prepareData(time: string, distance: string) {
+  const timeArray = time
+    .substring(time.indexOf(':') + 1)
+    .split(' ')
+    .filter((x) => x !== '');
+  const distanceArray = distance
+    .substring(distance.indexOf(':') + 1)
+    .split(' ')
+    .filter((x) => x !== '');
+  for (let i = 0; i < timeArray.length; i++) {
+    races.push({
+      time: parseInt(timeArray[i]),
+      distance: parseInt(distanceArray[i]),
+    });
+  }
+}
+
+function numberOfWaysToWinRace(time: number, distance: number) {
+  for (let i = 0; i <= time; i++) {
+    if (i * (time - i) > distance) {
+      return time + 1 - 2 * i;
+    }
+  }
 }
 
 const answer = await day6a();
--- puzzles/day-6/day-6-b.ts
+++ puzzles/day-6/day-6-b.ts
@@ -1,9 +1,38 @@
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
+let calculatedTime;
+let calculatedDistance;
+
 export async function day6b(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+  prepareData(data[0], data[1]);
+  console.log(calculatedTime, calculatedDistance);
+  return numberOfWaysToWinRace(
+    parseInt(calculatedTime),
+    parseInt(calculatedDistance)
+  );
+}
+
+function prepareData(time: string, distance: string) {
+  calculatedTime = time
+    .substring(time.indexOf(':') + 1)
+    .split(' ')
+    .filter((x) => x !== '')
+    .join('');
+  calculatedDistance = distance
+    .substring(distance.indexOf(':') + 1)
+    .split(' ')
+    .filter((x) => x !== '')
+    .join('');
+}
+
+function numberOfWaysToWinRace(time: number, distance: number) {
+  for (let i = 0; i <= time; i++) {
+    if (i * (time - i) > distance) {
+      return time + 1 - 2 * i;
+    }
+  }
 }
 
 const answer = await day6b();
